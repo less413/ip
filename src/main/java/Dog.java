583022fc51f2569b8e1db97e4c00df6668de9b6e
j <190504629+less413@users.noreply.github.com>
2026-08-15 12:26:57 +0800

    Rename chatbot from Duke to Dog

---
+++ src/main/java/Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+    public static void main(String[] args) {
+        String banner = "    _____                \n"
+                + "   |  __ \\  ____   ___ _ \n"
+                + "   | |  | |/ __ \\ / __' |\n"
+                + "   | |  | | |  | | |__| |\n"
+                + "   | |__| | |__| |\\___  |\n"
+                + "   |_____/ \\____/ ____/ |\n"
+                + "                  \\____/ \n";
+
+        System.out.println(banner);
+    }
+}
