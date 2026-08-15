8b83cc19b452ea87bf12944e306b6b09785dbb36
j <190504629+less413@users.noreply.github.com>
2026-08-15 12:58:47 +0800

    Add support for echo

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,19 +1,36 @@
+import java.util.Scanner;
+
 public class Dog {
     public static void main(String[] args) {
-        String line = "________________________________________________________________________________\n";
-        String banner = "    _____                \n"
-                + "   |  __ \\  ____   ___ _ \n"
-                + "   | |  | |/ __ \\ / __' |\n"
-                + "   | |  | | |  | | |__| |\n"
-                + "   | |__| | |__| |\\___  |\n"
-                + "   |_____/ \\____/ ____/ |\n"
-                + "                  \\____/ \n";
-        String greeting = "WOOF WOOF! How can I help? WOOF!";
+        String line = "    ____________________________________________________________\n";
+        String banner = "        _____                \n"
+                + "        |  __ \\  ____   ___ _ \n"
+                + "        | |  | |/ __ \\ / __' |\n"
+                + "        | |  | | |  | | |__| |\n"
+                + "        | |__| | |__| |\\___  |\n"
+                + "        |_____/ \\____/ ____/ |\n"
+                + "                       \\____/ \n";
+        String greeting = "      WOOF WOOF! How can I help? WOOF!";
 
-        System.out.println(line);
         System.out.println(banner);
         System.out.println(greeting);
-        
         System.out.println(line);
+        
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) continue;
+
+            System.out.println(line);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("      Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println("      " + input);
+                System.out.println(line);
+            }
+        }
     }
 }
