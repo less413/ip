7def84291f12e91d9da434888941fad8bc505cd8
j <190504629+less413@users.noreply.github.com>
2026-08-15 12:38:27 +0800

    Add greeting

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,5 +1,6 @@
 public class Dog {
     public static void main(String[] args) {
+        String line = "________________________________________________________________________________\n";
         String banner = "    _____                \n"
                 + "   |  __ \\  ____   ___ _ \n"
                 + "   | |  | |/ __ \\ / __' |\n"
@@ -7,7 +8,12 @@ public class Dog {
                 + "   | |__| | |__| |\\___  |\n"
                 + "   |_____/ \\____/ ____/ |\n"
                 + "                  \\____/ \n";
+        String greeting = "WOOF WOOF! How can I help? WOOF!";
 
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println(greeting);
+        
+        System.out.println(line);
     }
 }
