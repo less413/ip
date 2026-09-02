d129eb9edb09734552d6e88f15d057323c65c9f5
j <190504629+less413@users.noreply.github.com>
2026-09-02 17:23:44 +0800

    Add placeholder GUI window

--- src/main/java/dog/Main.java
+++ src/main/java/dog/Main.java
@@ -3,15 +3,35 @@ package dog;
 import java.util.Scanner;
 
 import dog.exceptions.DogException;
+import dog.ui.DialogBox;
 import dog.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 
 /**
- * Main entry point for the Dog task management application.
+ * Main entry point for the Dog application.
  */
-public class Main {
+public class Main extends Application {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final Dog dog = new Dog();
-    private static final Ui ui = new Ui();
+    private static final Dog DOG = new Dog();
+    private static final Ui UI = new Ui();
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private final Image USER_IMAGE = new Image(this.getClass().getResourceAsStream("/images/AvatarUser.png"));
+    private final Image DOG_IMAGE = new Image(this.getClass().getResourceAsStream("/images/AvatarDog.jpg"));
 
     /**
      * Orchestrates between user input, Dog processing, and UI display.
@@ -19,22 +39,77 @@ public class Main {
      * @param args command line arguments (not used).
      */
     public static void main(String[] args) {
-        ui.showDogMessage(dog.getWelcomeString());
+        UI.showDogMessage(DOG.getWelcomeString());
 
         while (SCANNER.hasNextLine()) {
             String input = SCANNER.nextLine();
             try {
-                Dog.CommandResult result = dog.processInput(input);
-                ui.showDogMessage(result.getMessage());
+                Dog.CommandResult result = DOG.processInput(input);
+                UI.showDogMessage(result.getMessage());
 
                 if (result.shouldExit()) {
                     break;
                 }
             } catch (DogException e) {
-                ui.showErrorMessage(e.getMessage());
+                UI.showErrorMessage(e.getMessage());
             }
         }
 
         SCANNER.close();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Setting up required components
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello!", USER_IMAGE);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        // Formatting the window to look as expected
+
+        stage.setTitle("i'm a dog woof woof");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // More code to be added here later
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
